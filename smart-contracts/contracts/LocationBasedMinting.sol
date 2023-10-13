a9a3d5e864131c00a42c4c6a17bc2e37eda9d354
Eason Chai <echai2905@gmail.com>
2023-10-13 16:09:47 +0800

    feat: Add location based NFT minting contract

---
+++ smart-contracts/contracts/LocationBasedMinting.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract LocationBasedMinting is ERC721 {
+    uint256 public tokenId = 0;
+    address public owner;
+    string public baseURI;
+
+    constructor(string memory _baseUri, address _owner) ERC721("Location-Based Proof Of Interaction", "POI") {
+        baseURI = _baseUri;
+        owner = _owner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can mint");
+        _;
+    }
+
+    function mint(address to) public onlyOwner {
+        uint256 _tokenId = tokenId;
+        tokenId += 1;
+        _mint(to, _tokenId);
+    }
+
+    function _baseURI() override internal view virtual returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory _baseURI) public onlyOwner {
+        baseURI = _baseURI;
+    }
+}
