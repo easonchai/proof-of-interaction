6255f21f1f66ead24de1083bbc3f48a8461084ef
Eason Chai <echai2905@gmail.com>
2023-10-14 15:05:55 +0800

    feat: Add hints to treasure hunt

--- frontend/utils/store.ts
+++ frontend/utils/store.ts
@@ -7,12 +7,16 @@ export interface Badge {
   name: string;
   imageUrl: string;
   tokenId?: string;
+  position?: number;
+  hint: string;
+  minted: boolean;
   collected: boolean;
 }
 
 interface GameState {
   badges: Badge[];
-  collect: (badge: { id: string; tokenId: string }) => void;
+  collect: (badge: { id: string; tokenId: string; position: number }) => void;
+  mint: (badge: { tokenId: string }) => void;
   reset: () => void;
 }
 
@@ -21,22 +25,31 @@ const defaultBadges: Badge[] = [
     id: "04441ADA811390",
     name: "ETHKL Nasi Lemak",
     imageUrl: "/assets/eth-tile.png",
+    hint: "Within the fiery venom's fold, the treasure guardian does hold.",
     tokenId: undefined,
     collected: false,
+    position: undefined,
+    minted: false,
   },
   {
     id: "2",
     name: "Taiko",
-    imageUrl: "/assets/mantle-tile.png",
+    imageUrl: "/assets/taiko-tile.png",
+    hint: "Where woven woods and cryptos meet, your prize lies subtly neat.",
     tokenId: undefined,
     collected: false,
+    position: undefined,
+    minted: false,
   },
   {
     id: "3",
     name: "Mantle",
-    imageUrl: "/assets/taiko-tile.png",
+    imageUrl: "/assets/mantle-tile.png",
+    hint: "Past the symbols of esteem, lies the treasure of your dream.",
     tokenId: undefined,
     collected: false,
+    position: undefined,
+    minted: false,
   },
 ];
 
@@ -50,6 +63,18 @@ const useGameStore = create<GameState>()(
             const found = state.badges.findIndex((b) => b.id === badge.id);
             if (found !== -1) {
               state.badges[found].collected = true;
+              state.badges[found].tokenId = badge.tokenId;
+              state.badges[found].position = badge.position;
+            }
+            return { badges: state.badges };
+          }),
+        mint: (badge) =>
+          set((state) => {
+            const found = state.badges.findIndex(
+              (b) => b.tokenId === badge.tokenId
+            );
+            if (found !== -1) {
+              state.badges[found].minted = true;
             }
             return { badges: state.badges };
           }),
