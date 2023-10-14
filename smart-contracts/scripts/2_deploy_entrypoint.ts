3a881fd7c292f38a2f305adc72538fc4c5790b16
Eason Chai <echai2905@gmail.com>
2023-10-14 19:36:32 +0800

    feat: Add deployment scripts

--- smart-contracts/scripts/2_deploy_entrypoint.ts
+++ smart-contracts/scripts/2_deploy_entrypoint.ts
@@ -1,21 +1,20 @@
 import { ethers, network } from "hardhat";
 
 async function main() {
-  if (
-    network.name !== "localhost" &&
-    network.name !== "hardhat" &&
-    network.name !== "running"
-  ) {
-    console.log("⏭ Skipping Set Balance [Not running locally]");
-    return;
-  }
-
   const entrypoint = await ethers.deployContract("EntryPoint", [], {
     gasLimit: 6e6,
   });
   await entrypoint.waitForDeployment();
 
   console.log(`✅ EntryPoint contract deployed to: ${entrypoint.target}\n`);
+
+  const AA = await ethers.getContractFactory("POIAccountFactory");
+  const aa = await AA.deploy(entrypoint.target, {
+    gasLimit: "0x1000000",
+  });
+  await aa.waitForDeployment();
+
+  console.log(`✅ POIAccountFactory contract deployed to: ${aa.target}\n`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- smart-contracts/scripts/3_deploy_poi.ts
+++ smart-contracts/scripts/3_deploy_poi.ts
@@ -1,13 +1,44 @@
 import { ethers, ethernal, network } from "hardhat";
 
+const generateRand = () => {
+  return Math.floor(Math.random() * 2000);
+};
+
+const generateMessage = async (seed: number, signer: any) => {
+  const data = (seed * generateRand()).toString();
+
+  const hash = ethers.id(`\x19Ethereum Signed Message:\n${data.length}${data}`);
+  const signature = await signer.signMessage(data);
+  return {
+    hash,
+    signature,
+  };
+};
+
 async function main() {
   const signer = await ethers.provider.getSigner();
   const address = await signer.getAddress();
+  const firstWallet = new ethers.Wallet(
+    process.env.PRIVATE_KEY || "",
+    ethers.provider
+  );
+  const secondWallet = new ethers.Wallet(
+    process.env.SECOND_PRIVATE_KEY || "",
+    ethers.provider
+  );
+  const thirdWallet = new ethers.Wallet(
+    process.env.THIRD_PRIVATE_KEY || "",
+    ethers.provider
+  );
   const poiVerifierUrl = "https://api.proofofinteraction.xyz/validation/";
   const nftBaseUri = "https://api.proofofinteraction.com/nft/";
-  const poi = await ethers.deployContract("ProofOfInteraction", [
-    poiVerifierUrl,
-  ]);
+  const poi = await ethers.deployContract(
+    "ProofOfInteraction",
+    [poiVerifierUrl],
+    {
+      gasLimit: "0x1000000",
+    }
+  );
   await poi.waitForDeployment();
 
   if (
@@ -22,14 +53,18 @@ async function main() {
   }
   console.log(`🧪 Proof of Interaction contract deployed to: ${poi.target}\n`);
 
-  const oracle = await ethers.deployContract("Oracle", [[address]]);
+  const oracle = await ethers.deployContract("Oracle", [[address]], {
+    gasLimit: "0x1000000",
+  });
   await oracle.waitForDeployment();
   console.log(`🔎 Oracle contract deployed to: ${oracle.target}\n`);
 
   const LocationBasedMinting = await ethers.getContractFactory(
     "LocationBasedMinting"
   );
-  const nft = await LocationBasedMinting.deploy(nftBaseUri, address);
+  const nft = await LocationBasedMinting.deploy(nftBaseUri, address, {
+    gasLimit: "0x1000000",
+  });
   await nft.waitForDeployment();
   console.log(
     `📍 Location-based Minting NFT contract deployed to: ${nft.target}\n`
@@ -44,21 +79,68 @@ async function main() {
 
   // signer.sendTransaction(tx);
 
-  await poi.setNFTAddress(nft.target);
-  await poi.setOracleAddress(oracle.target);
-  await nft.setOwner(address);
-  await nft.setMinter(poi.target);
+  await poi.setNFTAddress(nft.target, {
+    gasLimit: "0x1000000",
+  });
+  await poi.setOracleAddress(oracle.target, {
+    gasLimit: "0x1000000",
+  });
+  await nft.setOwner(address, {
+    gasLimit: "0x1000000",
+  });
+  await nft.setMinter(poi.target, {
+    gasLimit: "0x1000000",
+  });
   console.log("✅ Proof of Interaction contract configured\n");
 
   console.log(
-    `npx hardhat verify --network opTest ${poi.target} ${poiVerifierUrl}`
+    `npx hardhat verify --network ${network} ${poi.target} ${poiVerifierUrl}`
   );
   console.log(
-    `npx hardhat verify --network opTest --constructor-args utils/oracle.arguments.js ${oracle.target}`
+    `npx hardhat verify --network ${network} --constructor-args utils/oracle.arguments.js ${oracle.target}`
   );
   console.log(
-    `npx hardhat verify --network opTest ${nft.target} "${nftBaseUri}" "${address}"`
+    `npx hardhat verify --network ${network} ${nft.target} "${nftBaseUri}" "${address}"`
   );
+
+  if (network.name === "mantleTest" || network.name === "taiko") {
+    // AA is not live yet!
+    // Users need to save interaction data to the AA
+
+    const pickRandomWallet = () => {
+      const index = Math.floor(Math.random() * 3);
+      switch (index) {
+        case 0:
+          return firstWallet;
+        case 1:
+          return secondWallet;
+        case 2:
+          return thirdWallet;
+        default:
+          return firstWallet;
+      }
+    };
+
+    const randomData = new Date().getTime();
+    for (let i = 0; i < 35; i++) {
+      const wallet = pickRandomWallet();
+      const message = await generateMessage(randomData, wallet);
+      await poi.saveInteraction(message.hash, message.signature, {
+        gasLimit: "0x1000000",
+      });
+      await oracle.saveValidationRequest(
+        `https://api.proofofinteraction.xyz/validation/${message.hash}`,
+        message.hash,
+        true,
+        {
+          gasLimit: "0x1000000",
+        }
+      );
+      await poi.selectWinner(message.hash, {
+        gasLimit: "0x1000000",
+      });
+    }
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
